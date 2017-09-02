b0294fc41c2ae0b4ba89d306837011c19cb8552c
José Tapia <jose.tapiaav@gmail.com>
2017-09-01 20:25:59 -0500

    Mejorar el intercambio y prueba

--- bubblesort.py
+++ bubblesort.py
@@ -6,8 +6,13 @@ def bubblesort(arr):
     for i in range(len(arr)):
         for j in range(0,len(arr)-i-1):
             if(aux[j]>aux[j+1]):
-                w=aux[j]
-                aux[j]=aux[j+1]
-                aux[j+1]=w
+                aux[j],aux[j+1]=aux[j+1],aux[j]
                 cantidad+=1
     return aux
+
+import random
+p = random.sample(range(0,200),100)
+print(p)
+psorted=bubblesort(p)
+print(cantidad)
+print(psorted)
