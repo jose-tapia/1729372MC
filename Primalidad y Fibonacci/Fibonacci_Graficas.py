fc3cc91d69b22f0657d1ac9e0bc6d38d90facbe9
José Tapia <jose.tapiaav@gmail.com>
2017-10-12 20:59:21 -0500

    Create Fibonacci_Graficas.py

---
+++ Primalidad y Fibonacci/Fibonacci_Graficas.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+
+memo={}
+cnt=0
+
+def fibonacci(n):
+    global memo,cnt
+    cnt+=1
+    if n==0 or n==1:
+        return 1
+    if n in memo:
+        return memo[n]
+    else:
+        memo[n]=fibonacci(n-2)+fibonacci(n-1)
+        return memo[n]
+
+
+def fiborec(n):
+    global cnt
+    cnt+=1
+    if n==0 or n==1:
+        return 1
+    return fiborec(n-1)+fiborec(n-2)
+
+def fiboiter(n):
+    global cnt
+    fib=[1,1]
+    cnt+=1
+    for k in range(2,n+1):
+        cnt+=1
+        fib.append(fib[k-1]+fib[k-2])
+    return fib[n]
+
+
+#Hasta 50
+maximo=50
+rango=range(0,maximo)
+cntr,cnti,cntm=[],[],[]
+for n in rango:
+    cnt=0
+    a=fiborec(n)
+    cntr.append(cnt)
+    cnt=0
+    b=fiboiter(n)
+    cnti.append(cnt)
+    memo,cnt={},0
+    c=fibonacci(n)
+    cntm.append(cnt)
+
+
+plt.axis([0,50,0,100])
+plt.plot(rango,cnti,label="Iterativo")
+plt.plot(rango,cntr,label="Recursivo")
+plt.plot(rango,cntm,label="Memoria")
+plt.xlabel("Número de fibonacci")
+plt.ylabel("Cantidad de operaciones")
+plt.title("Sucesión de Fibonacci")
+plt.legend()
+plt.savefig("fibo4.png")
+plt.show()
