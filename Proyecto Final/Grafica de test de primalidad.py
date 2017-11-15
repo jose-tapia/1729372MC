4fc6256473a3b83f5d87728ead3957f069d06f45
José Tapia <jose.tapiaav@gmail.com>
2017-11-14 22:57:53 -0600

    Create Grafica de test de primalidad.py

---
+++ Proyecto Final/Grafica de test de primalidad.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np 
+
+def esPrimo(n):
+    cnt=0
+    if n<=1:
+        return cnt
+    m=2
+    while(m*m<=n):
+        cnt+=1
+        if n%m==0:
+            return cnt
+        m+=1
+    return cnt
+
+cntprm=[esPrimo(x) for x in range(1,100001,1000)]
+
+x=np.linspace(1,100000)
+plt.plot(range(1,100001,1000),cntprm, label="Test de primalidad")
+plt.plot(x,x**(0.5),label="Raíz de n",color="green")
+plt.xlabel('Número')
+plt.ylabel('Cantidad de operaciones')
+plt.title('Test de primalidad')
+plt.legend()
+plt.savefig("test.png")
+plt.show()
