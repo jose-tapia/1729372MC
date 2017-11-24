e3c3acdd5c778de752022456da4b3294c51482a6
José Tapia <jose.tapiaav@gmail.com>
2017-11-24 16:08:33 -0600

    Graficas de  a verdis

--- Proyecto Final/GraficasProyectoFinal.py
+++ Proyecto Final/GraficasProyectoFinal.py
@@ -6,7 +6,7 @@ def esPrimo(n):
     if n<=1:
         return cnt
     m=2
-    while m<n:
+    while m*m<=n:
         cnt+=1
         if n%m==0:
             return cnt
@@ -24,6 +24,8 @@ def itsPrime(n):
     return True
 
 def graficar(xs,ys,imagen,titulo):
+    x=np.linspace(1,100000)
+    plt.plot(x,x**(0.5),label="Raíz de n",color="green")
     plt.plot(xs,ys, label=titulo)
     plt.xlabel("Número")
     plt.ylabel("Cantidad de operaciones")
@@ -32,60 +34,29 @@ def graficar(xs,ys,imagen,titulo):
     plt.savefig(imagen)
     plt.show()
 
-esprimo=[esPrimo(x) for x in range(100001)]
-itsprime=[itsPrime(x) for x in range(100001)]
-
-
 cnt=251
 
 G1xs=range(1,100001,cnt)
-G1ys=[esprimo[x] for x in G1xs]
+G1ys=[esPrimo(x) for x in G1xs]
 
 G2xs=range(1,50001,cnt)
-G2ys=[esprimo[x] for x in G2xs]
+G2ys=[esPrimo(x) for x in G2xs]
 
 G3xs=range(50000,100001,cnt)
-G3ys=[esprimo[x] for x in G3xs]
+G3ys=[esPrimo(x) for x in G3xs]
 
 G4xs=[x for x in range(1,100001,cnt) if x%2==0]
-G4ys=[esprimo[x] for x in G4xs]
+G4ys=[esPrimo(x) for x in G4xs]
 
-G5xs=[x for x in range(1,100001,cnt) if x%2==1 if not itsprime[x]]
-G5ys=[esprimo[x] for x in G5xs]
+G5xs=[x for x in range(1,100001,cnt) if x%2==1 if not itsPrime(x)]
+G5ys=[esPrimo(x) for x in G5xs]
 
-G6xs=[x for x in range(1,100001,cnt) if itsprime[x]]
-G6ys=[esprimo[x] for x in G6xs]
+G6xs=[x for x in range(1,100001,cnt) if itsPrime(x)]
+G6ys=[esPrimo(x) for x in G6xs]
 
-graficar(G1xs,G1ys,"1Numeros1al100000.png","Complejidad por número")
-graficar(G2xs,G2ys,"1Numeros1al50000.png","Complejidad por número")
-graficar(G3xs,G3ys,"1Numeros50000al100000.png","Complejidad por número")
-graficar(G4xs,G4ys,"1NumerosPares.png","Complejidad por número par")
-graficar(G5xs,G5ys,"1NumerosImpares.png","Complejidad por número impar")
-graficar(G6xs,G6ys,"1NumerosPrimos.png","Complejidad por número primo")
-
-cnt=1
-
-G1xs=range(1,100001,cnt)
-G1ys=[esprimo[x] for x in G1xs]
-
-G2xs=range(1,50001,cnt)
-G2ys=[esprimo[x] for x in G2xs]
-
-G3xs=range(50000,100001,cnt)
-G3ys=[esprimo[x] for x in G3xs]
-
-G4xs=[x for x in range(1,100001,cnt) if x%2==0]
-G4ys=[esprimo[x] for x in G4xs]
-
-G5xs=[x for x in range(1,100001,cnt) if x%2==1 if not itsprime[x]]
-G5ys=[esprimo[x] for x in G5xs]
-
-G6xs=[x for x in range(1,100001,cnt) if itsprime[x]]
-G6ys=[esprimo[x] for x in G6xs]
-
-graficar(G1xs,G1ys,"2Numeros1al100000.png","Complejidad por número")
-graficar(G2xs,G2ys,"2Numeros1al50000.png","Complejidad por número")
-graficar(G3xs,G3ys,"2Numeros50000al100000.png","Complejidad por número")
-graficar(G4xs,G4ys,"2NumerosPares.png","Complejidad por número par")
-graficar(G5xs,G5ys,"2NumerosImpares.png","Complejidad por número impar")
-graficar(G6xs,G6ys,"2NumerosPrimos.png","Complejidad por número primo")
+graficar(G1xs,G1ys,"Numeros1al100000.png","Complejidad por número")
+graficar(G2xs,G2ys,"Numeros1al50000.png","Complejidad por número")
+graficar(G3xs,G3ys,"Numeros50000al100000.png","Complejidad por número")
+graficar(G4xs,G4ys,"NumerosPares.png","Complejidad por número par")
+graficar(G5xs,G5ys,"NumerosImpares.png","Complejidad por número impar")
+graficar(G6xs,G6ys,"NumerosPrimos.png","Complejidad por número primo")
