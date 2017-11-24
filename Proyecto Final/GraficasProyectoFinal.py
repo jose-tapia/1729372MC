8b33c1d172911aff64d3b04ee911a7c981af00e9
José Tapia <jose.tapiaav@gmail.com>
2017-11-24 17:52:28 -0600

    Todas las graficas

--- Proyecto Final/GraficasProyectoFinal.py
+++ Proyecto Final/GraficasProyectoFinal.py
@@ -35,28 +35,25 @@ def graficar(xs,ys,imagen,titulo):
     plt.show()
 
 cnt=251
-
 G1xs=range(1,100001,cnt)
 G1ys=[esPrimo(x) for x in G1xs]
-
 G2xs=range(1,50001,cnt)
 G2ys=[esPrimo(x) for x in G2xs]
-
 G3xs=range(50000,100001,cnt)
 G3ys=[esPrimo(x) for x in G3xs]
-
 G4xs=[x for x in range(1,100001,cnt) if x%2==0]
 G4ys=[esPrimo(x) for x in G4xs]
-
-G5xs=[x for x in range(1,100001,cnt) if x%2==1 if not itsPrime(x)]
+G5xs=[x for x in range(1,100001,cnt) if x%2==1]
 G5ys=[esPrimo(x) for x in G5xs]
-
-G6xs=[x for x in range(1,100001,cnt) if itsPrime(x)]
+G6xs=[x for x in range(1,100001,cnt) if x%2==1 if not itsPrime(x)]
 G6ys=[esPrimo(x) for x in G6xs]
+G7xs=[x for x in range(1,100001,cnt) if itsPrime(x)]
+G7ys=[esPrimo(x) for x in G7xs]
 
-graficar(G1xs,G1ys,"Numeros1al100000.png","Complejidad por número")
-graficar(G2xs,G2ys,"Numeros1al50000.png","Complejidad por número")
-graficar(G3xs,G3ys,"Numeros50000al100000.png","Complejidad por número")
-graficar(G4xs,G4ys,"NumerosPares.png","Complejidad por número par")
-graficar(G5xs,G5ys,"NumerosImpares.png","Complejidad por número impar")
-graficar(G6xs,G6ys,"NumerosPrimos.png","Complejidad por número primo")
+graficar(G1xs,G1ys,"Numeros1al100000.png","Complejidad por numero")
+graficar(G2xs,G2ys,"Numeros1al50000.png","Complejidad por numero")
+graficar(G3xs,G3ys,"Numeros50000al100000.png","Complejidad por numero")
+graficar(G4xs,G4ys,"NumerosPares.png","Complejidad por numero par")
+graficar(G5xs,G5ys,"NumerosImpares.png","Complejidad por numero impar")
+graficar(G6xs,G6ys,"NumerosImparesnoPrimo.png","Complejidad por numero impar")
+graficar(G7xs,G7ys,"NumerosPrimos.png","Complejidad por numero primo")
